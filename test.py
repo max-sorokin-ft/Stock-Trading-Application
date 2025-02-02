7c140b7bef71b03636b8c764b110de2e27405260
Max Sorokin <156485161+max-sorokin-ft@users.noreply.github.com>
2025-02-02 13:43:56 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas_market_calendars as mcal
-from datetime import datetime, timedelta
-
-nyse = mcal.get_calendar('NYSE')
-end_date = datetime.now()
-start_date = end_date - timedelta(days=10)
-valid_days = nyse.valid_days(start_date=start_date, end_date=end_date)
-
-print(valid_days[-1])
